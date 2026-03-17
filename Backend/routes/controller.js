9fa07f55a15dff6a4a24d45f1ffd76dd82ffb5b8
Bhupinder Singh <bhupinderdhiman312@gmail.com>
2026-03-17 14:18:24 +0530

    update navbar

--- Backend/routes/controller.js
+++ Backend/routes/controller.js
@@ -40,8 +40,23 @@ router.post("/register", async (req, res) => {
         const hashPassword = await bcrypt.hash(password, 10)
         const newUser = new User({ name, email, password: hashPassword })
         await newUser.save()
-        res.status(200).json({ message: "Seccessfully Registered" })
-    } catch (error) {
+const token = jwt.sign(
+      { id: newUser._id, role: newUser.role, name: newUser.name },
+      process.env.Key,
+      { expiresIn: "1h" },
+    );
+
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    });
+
+    res.json({
+      message: "Register succesfully",
+      token,
+      role: newUser.role,
+    });    } catch (error) {
         res.status(500).json({
             message: error.message
         })
@@ -68,8 +83,8 @@ router.post("/login", async (req, res) => {
     });
         res.cookie("token", token,{
           httpOnly:true,
-          secure:false,
-          sameSite:"Lax",
+          secure:true,
+          sameSite:"none",
         })
         // console.log(token)
         res.status(200).json({ message: "Login Seccessfully" , token, role:newUser.role})
@@ -121,8 +136,8 @@ router.post("/admin",authMiddleware,checkAdmin,upload.single("image"), async (re
 router.post("/logout", async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: false,   // true if HTTPS
-    sameSite: "Lax", // must match login
+    secure: true,   // true if HTTPS
+    sameSite: "none", // must match login
   });
   res.json({ message: "Logged out successfully" });
 });
