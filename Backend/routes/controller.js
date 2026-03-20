ec0c1e968d19129da3fb54f27b20b0d51111fd21
Bhupinder Singh <bhupinderdhiman312@gmail.com>
2026-03-20 13:58:48 +0530

    update routers

--- Backend/routes/controller.js
+++ Backend/routes/controller.js
@@ -195,14 +195,14 @@ router.post("/buynow/add", async (req, res) => {
   }
 });
 
-router.get("/buynow", async (req, res) => {
-  try {
-    const orders = await Order.find();
-    res.status(200).json(orders);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+// router.get("/buynow", async (req, res) => {
+//   try {
+//     const orders = await Order.find();
+//     res.status(200).json(orders);
+//   } catch (err) {
+//     res.status(500).json({ message: err.message });
+//   }
+// });
 
 router.get("/orders", async (req, res) => {
    try {
