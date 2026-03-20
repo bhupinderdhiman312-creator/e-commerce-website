282690fdf8b442e46c8c5b92b272c52ae84e5852
Bhupinder Singh <bhupinderdhiman312@gmail.com>
2026-03-20 15:29:13 +0530

    update routers

--- frontend/src/component/Join.jsx
+++ frontend/src/component/Join.jsx
@@ -27,7 +27,7 @@ function Join() {
     try {
       const res = await axios.post(
         "https://e-commerce-website-6yh3.onrender.com/api/register",
-        data
+        data,{withCredentials: true}
       );
        const role = res.data.role;
       const token = res.data.token;
